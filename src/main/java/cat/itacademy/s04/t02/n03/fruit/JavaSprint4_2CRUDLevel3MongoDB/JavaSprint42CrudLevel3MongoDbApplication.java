020583fd729a9ac5612591dccd72195754f0fd8a
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-14 20:50:25 +0100

    feat: initial commit

---
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/JavaSprint4_2CRUDLevel3MongoDB/JavaSprint42CrudLevel3MongoDbApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n03.fruit.JavaSprint4_2CRUDLevel3MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSprint42CrudLevel3MongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSprint42CrudLevel3MongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/s04/t02/n03/fruit/JavaSprint4_2CRUDLevel3MongoDB/JavaSprint42CrudLevel3MongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s04.t02.n03.fruit.JavaSprint4_2CRUDLevel3MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSprint42CrudLevel3MongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
