1a5e44570677a351209f6a76709423f0511b0d0e
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-16 20:14:53 +0100

    feat: add controller

---
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/JavaSprint4_2CRUDLevel3MongoDB/controller/OrderController.java
@@ -0,0 +1,28 @@
+package cat.itacademy.s04.t02.n03.fruit.JavaSprint4_2CRUDLevel3MongoDB.controller;
+
+import cat.itacademy.s04.t02.n03.fruit.JavaSprint4_2CRUDLevel3MongoDB.DTO.OrderRequest;
+import cat.itacademy.s04.t02.n03.fruit.JavaSprint4_2CRUDLevel3MongoDB.DTO.OrderResponse;
+import cat.itacademy.s04.t02.n03.fruit.JavaSprint4_2CRUDLevel3MongoDB.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public OrderResponse createOrder(
+            @RequestBody @Valid OrderRequest request
+            ) {
+        return orderService.createOrder(request);
+
+    }
+
+
+
+}
