0f4dcade2c1d89a541bc65623bacdcf1a22b1891
jrffruit518-enrich <jrffruit518@gmail.com>
2026-01-17 20:11:39 +0100

    fix:update OrderRequest

--- src/main/java/cat/itacademy/s04/t02/n03/fruit/JavaSprint4_2CRUDLevel3MongoDB/DTO/OrderRequest.java
+++ src/main/java/cat/itacademy/s04/t02/n03/fruit/JavaSprint4_2CRUDLevel3MongoDB/DTO/OrderRequest.java
@@ -1,6 +1,7 @@
 package cat.itacademy.s04.t02.n03.fruit.JavaSprint4_2CRUDLevel3MongoDB.DTO;
 
 import cat.itacademy.s04.t02.n03.fruit.JavaSprint4_2CRUDLevel3MongoDB.entities.Fruit;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
@@ -12,6 +13,7 @@ public record OrderRequest(
         @NotBlank (message = "Client name is required")
         String clientName,
         @NotEmpty(message = "Fruit list cannot be empty")
+        @Valid
         List<Fruit> fruitList,
         @NotNull(message = "Delivery date is required")
         LocalDate deliveryDate
